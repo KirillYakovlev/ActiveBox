ddff0dff898455db0914322f23de4c14de12168d
kirilyakovlev69 <62206830+kirilyakovlev69@users.noreply.github.com>
2020-05-17 00:17:48 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,76 @@
+$(function () {
+
+    /* Fixed Header */
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    checkSkroll(scrollPos, introH);
+
+    $(window).on("scroll load resize", function() {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+        checkSkroll(scrollPos, introH);
+    });
+
+    function checkSkroll(scrollPos, introH) {
+        if( scrollPos > introH ) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+    /* Smooth Scroll */
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementId = $(this). data ("scroll");
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+           scrollTop: elementOffset - 35
+        } ,1000 );
+    });
+
+    /* NavToggle */
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
+    /* Reviews https://kenwheeler.github.io/slick/ */
+        let slider = $("#reviewsSlider");
+        slider.slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            arrows: false,
+            dots: true
+        });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
